beef7e90e43045b2ac1371f99abaea48c313807e
Nbryan11 <108432413+Nbryan11@users.noreply.github.com>
2024-12-27 15:29:37 -0500

    Update HelloController.java

--- src/main/java/com/brayanjenkins/springjenkins/Controller/HelloController.java
+++ src/main/java/com/brayanjenkins/springjenkins/Controller/HelloController.java
@@ -3,7 +3,7 @@ package com.brayanjenkins.springjenkins.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestControllerr
 public class HelloController {
 
     @GetMapping("/welcome")
